c9310b0c01add5fc1401237d0399701ecffb4bb6
Rohitkanyal <kanyalrohit@gmail.com>
2016-12-27 14:33:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var path = require('path');
 
 var Pool = require('pg').Pool;
 
-var config = {
+var config - {
     user:'rohitkanyal',
     database:'rohitkanyal',
     host: 'db.imad.hasura-app.io',
