12b668ff0aa4f4fdfad12716b1cc9060a26757d6
Rohitkanyal <kanyalrohit@gmail.com>
2016-12-28 18:58:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,45 +1,37 @@
-// Counter code
-var button = document.getElementById('counter');
 
-button.onclick = function() {
-     
-     //CREATE A REQUEST OBJECT
-     var request = new XMLHttpRequest();
-     // Capture the response and store it in a variable
-     request.onreadystatechage = function() 
-     {
-         if (request.readyState --- XMLHttpRequest.DONE)
-         {
-             //take some action
-              if(request.status === 200)
-              {
-                  var counter = request.responseText;
-                  var span = document.getElementById('count');
-                  span.innerHTML=counter.toString();
-              }    
-         }
-             //NOtdone et
-             
-     };
-     //MAKE THE REQUEST
-     request.open('GET')
-    
-};
-//Submit name
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
+//Submit username/password to login
+
 var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
     
-    //Make a request to the serer and send the name
+       //Creat a request object
+       var request = new XMLHttpRequest();
+       
+       // Catputre the response and store in variable
+       
+    if (request.readyState ===XMLHttpRequst.DONE){
+    // take some action
+    if (request.readyState === 200){
+       // Capture a list of names and render it as a list
+       console.log('user logged in');
+       alert('Logged in successfully');
+     }else if(request.status ===403){
+         alert('Username/password is incorrect');
+     }
+     else if(request.status ===500){
+           alert('something went wrong o the server');
+     }
     
-    
-    // Capture a list of names and render it as a list
-    var names =['name1','name2','name3','name4'];
-    var list ='';
-    for (var i=0;i<names.length;i++){
-        list+='<li>'+names[i]+'</li>';
-    }
-    var ul =document.getElementById('namelist');
-    ul.innerHTML =list;
+     }
+     // not done yet
+   };
+
+    //Make the request
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    console.log(username);
+    console.log(password);
+    request.open('POST', 'http://http://rohitkanyal.imad.hasura-app.io/login', true);
+    request.setRequestHeader('Content-Type','application/json');
+    request.send(JSON.stringify({username:username,password:}));
 };
